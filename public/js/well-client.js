85a52a9f35485686fcff18e84c6be439107a1f92
wangduanduan <1779706607@qq.com>
2017-07-18 18:20:34 +0800

    0

--- public/js/well-client.js
+++ public/js/well-client.js
@@ -21,7 +21,7 @@ var wellClient = (function($) {
     };
 
     var Config = {
-        version: '2.7.5',
+        version: '2.7.18',
 
         SDK: 'mbsdk.wellcloud.cc',
         cstaPort: ':5088',
