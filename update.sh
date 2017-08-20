eec59c30ee5d8432fc5f3bdaf42c34575e9a0063
wangduanduan <1779706607@qq.com>
2017-08-20 15:54:53 +0800

    0

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # config the version of wellclient
-UPDATE_VERSION=v2.7.18.RELEASE
+UPDATE_VERSION=v2.8.20.RELEASE
 
 git fetch origin ${UPDATE_VERSION}:${UPDATE_VERSION}
+git ci -am "update the wellclient version"
 git checkout ${UPDATE_VERSION}
